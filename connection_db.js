895a962366fbd9f01764798f12e2f6538f5708dd
unknown <diegomarmiroli20@gmail.com>
2023-07-22 14:55:41 -0300

    Finalizado los endpoints con sus funcionalidades

--- connection_db.js
+++ connection_db.js
@@ -5,6 +5,10 @@ require("dotenv").config({ path: path.join(__dirname, ".env") });
 
 const client = new MongoClient(process.env.DATABASE_URL);
 
+/**
+ * Crea conexión a la base de datos en mongo
+ * @returns MongoDB Connection
+ */
 async function connect() {
     try {
         return await client.connect();
@@ -14,6 +18,9 @@ async function connect() {
     }
 }
 
+/**
+ * Desconecta el cliente conectado
+ */
 async function disconnect() {
     try {
         await client.close();
@@ -22,5 +29,29 @@ async function disconnect() {
         throw error;
     }
 }
+/**
+ *
+ * @param {String} collectionName Colección a la que se conectará
+ * @returns Retorna la colección a la que realizar las sentencias
+ */
+async function connectToCollection(collectionName) {
+    const connection = await connect();
+    const db = connection.db(process.env.DATABASE_NAME);
+    const collection = db.collection(collectionName);
 
-module.exports = { connect, disconnect };
+    return collection;
+}
+
+/**
+ * Ordena el id de mayor a menor, aumenta el primer id en 1 y retorna este id
+ * @param {Class} collection Elemento colección obtenida de una conexión
+ * @returns {Int}
+ */
+async function generateId(collection) {
+    const documentMaxId = await collection.find().sort({ codigo: -1 }).limit(1).toArray();
+    const maxCodigo = documentMaxId[0]?.codigo ?? 0;
+
+    return maxCodigo + 1;
+}
+
+module.exports = { connectToCollection, disconnect, generateId };
--- src/server.js
+++ src/server.js
@@ -2,7 +2,120 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+const { connectToCollection, generateId, disconnect } = require("../connection_db");
+
 require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/v1/muebles", async (req, res) => {
+    try {
+        const { categoria, precio_lte, precio_gte } = req.query;
+
+        const conn = await connectToCollection("muebles");
+
+        const query = {};
+        const sort = {};
+
+        if (categoria) {
+            query.categoria = categoria;
+            sort.nombre = 1;
+        }
+        if (precio_lte) {
+            if (isNaN(Number(precio_lte))) throw new Error();
+            query.precio = { ...query.precio, $lte: Number(precio_lte) };
+            sort.precio = -1;
+        }
+        if (precio_gte) {
+            if (isNaN(Number(precio_gte))) throw new Error();
+            query.precio = { ...query.precio, $gte: Number(precio_gte) };
+            sort.precio = 1;
+        }
+
+        let muebles = await conn.find(query).sort(sort).toArray();
+        res.status(200).send(JSON.stringify({ payload: muebles }));
+    } catch (error) {
+        res.status(500).send(JSON.stringify({ message: "Se ha generado un error en el servidor" }));
+    }
+});
+app.get("/api/v1/muebles/:codigo", async (req, res) => {
+    try {
+        const { codigo } = req.params;
+
+        if (!codigo || isNaN(Number(codigo))) throw new Error("El codigo es inválido");
+
+        const conn = await connectToCollection("muebles");
+
+        let mueble = await conn.findOne({ codigo: Number(codigo) });
+
+        if (!mueble) return res.status(400).send(JSON.stringify({ message: "El código no corresponde a un mueble registrado" }));
+
+        res.status(200).send(JSON.stringify({ payload: mueble }));
+    } catch (error) {
+        res.status(500).send(JSON.stringify({ message: "Se ha generado un error en el servidor" }));
+    }
+});
+app.post("/api/v1/muebles", async (req, res) => {
+    try {
+        const { categoria, nombre, precio } = req.body;
+
+        if (!categoria || categoria.trim().length < 1) return res.status(400).send(JSON.stringify({ message: "Faltan datos relevantes" }));
+        if (!nombre || nombre.trim().length < 1) return res.status(400).send(JSON.stringify({ message: "Faltan datos relevantes" }));
+        if (!precio || isNaN(Number(precio))) return res.status(400).send(JSON.stringify({ message: "Faltan datos relevantes" }));
+
+        const conn = await connectToCollection("muebles");
+        let data = { categoria, nombre, precio: Number(precio), codigo: await generateId(conn) };
+        let mueble = await conn.insertOne(data);
+        if (!mueble) throw new Error("Error al insertar el mueble");
+
+        res.status(201).send(JSON.stringify({ message: "Registro creado", payload: data }));
+    } catch (error) {
+        res.status(500).send(JSON.stringify({ message: "Se ha generado un error en el servidor" }));
+    }
+});
+app.put("/api/v1/muebles/:codigo", async (req, res) => {
+    try {
+        const { codigo } = req.params;
+        const { categoria, nombre, precio } = req.body;
+
+        if (!codigo || isNaN(Number(codigo))) return res.status(400).send(JSON.stringify({ message: "Faltan datos relevantes" }));
+        if (!categoria || categoria.trim().length < 1) return res.status(400).send(JSON.stringify({ message: "Faltan datos relevantes" }));
+        if (!nombre || nombre.trim().length < 1) return res.status(400).send(JSON.stringify({ message: "Faltan datos relevantes" }));
+        if (!precio || isNaN(Number(precio))) return res.status(400).send(JSON.stringify({ message: "Faltan datos relevantes" }));
+
+        const conn = await connectToCollection("muebles");
+
+        let mueble = await conn.findOne({ codigo: Number(codigo) });
+        if (!mueble) return res.status(400).send(JSON.stringify({ message: "El código no corresponde a un mueble registrado" }));
+
+        mueble = { ...mueble, categoria, nombre, precio: Number(precio) };
+        await conn.updateOne({ codigo: Number(codigo) }, { $set: mueble });
+
+        res.status(200).send(JSON.stringify({ message: "Registro actualizado", payload: mueble }));
+    } catch (error) {
+        res.status(500).send(JSON.stringify({ message: "Se ha generado un error en el servidor" }));
+    }
+});
+app.delete("/api/v1/muebles/:codigo", async (req, res) => {
+    try {
+        const { codigo } = req.params;
+
+        if (!codigo || isNaN(Number(codigo))) return res.status(400).send(JSON.stringify({ message: "Faltan datos relevantes" }));
+
+        const conn = await connectToCollection("muebles");
+
+        if (!(await conn.findOne({ codigo: Number(codigo) }))) return res.status(400).send(JSON.stringify({ message: "El código no corresponde a un mueble registrado" }));
+
+        await conn.deleteOne({ codigo: Number(codigo) });
+
+        res.status(200).send(JSON.stringify({ message: "Registro eliminado" }));
+    } catch (error) {
+        res.status(500).send(JSON.stringify({ message: "Se ha generado un error en el servidor" }));
+    }
+});
+app.use("*", (req, res) => {
+    res.status(404).send("No hay nada");
+});
 app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => console.log(`Example app listening on port ${process.env.SERVER_PORT}!`));
